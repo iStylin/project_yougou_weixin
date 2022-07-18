593f305dc5c459480c1e3db801c5c0acb5c949b7
iStylin <480881933@qq.com>
2022-07-18 09:44:59 +0800

    finish tabbar

---
+++ pages/home/home.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		home
+	</view>
+</template>
+
+<script>
+	export default {
+		name:'home',
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
